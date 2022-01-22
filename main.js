5aea4a2e2ebaa8dc4793ac508ed64457a2ca11d9
大山和徳 <kazunori-oyama@tonichi-printing.co.jp>
2022-01-22 23:36:29 +0900

    answer for L5

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ const app = Vue.createApp({
       image: "./assets/images/socks_blue.jpg",
       inventory: 0,
       onSale: true,
+      details: ["50% cotton", "30% wool", "10% polyester"],
+      sizes: ["S", "M", "L"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+        },
+        {
+          id: 2235,
+          color: "blue",
+        },
+      ],
     };
   },
 });
