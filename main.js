492aeec2dcb6c7e8346ad40102bf5d9663972b6c
大山和徳 <kazunori-oyama@tonichi-printing.co.jp>
2022-01-23 21:33:48 +0900

    download files for starting

--- main.js
+++ main.js
@@ -1,52 +1,34 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      cart: 0,
-      product: "Socks",
-      onSale: false,
-      brand: "Vue Mastery",
-      selectedVariant: 0,
-      details: ["50% cotton", "30% wool", "10% polyester"],
-      sizes: ["S", "M", "L"],
-      variants: [
-        {
-          id: 2234,
-          color: "green",
-          image: "./assets/images/socks_green.jpg",
-          quantity: 50,
+    data() {
+        return {
+            cart: 0,
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
         },
-        {
-          id: 2235,
-          color: "blue",
-          image: "./assets/images/socks_blue.jpg",
-          quantity: 0,
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
         },
-      ],
-    };
-  },
-  methods: {
-    addToCart() {
-      this.cart += 1;
-    },
-    removeFromCart() {
-      this.cart -= 1;
-    },
-    updateVariant(index) {
-      this.selectedVariant = index;
-    },
-  },
-  computed: {
-    title() {
-      return this.brand + " " + this.product;
-    },
-    onSaleTitle() {
-      return this.brand + " " + this.product + "is OnSale Now.";
-    },
-    image() {
-      return this.variants[this.selectedVariant].image;
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].quantity;
-    },
-  },
-});
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].image
+        }
+    }
+})
